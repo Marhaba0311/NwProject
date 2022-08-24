f440298a605ac2d10e2620d2d4fc196088b9834a
Dilixiati Mailihaba <marhaba311@hotmail.com>
2022-08-24 11:44:31 -0400

    Added commit

--- src/Project/Project1.java
+++ src/Project/Project1.java
@@ -4,6 +4,7 @@ public class Project1 {
     public static void main(String[] args) {
         // project started
         System.out.println("Hello World");
+        System.out.println("Project");
 
     }
 }
